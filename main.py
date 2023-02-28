07a8cf1e927f5fac25624089c27fb05c3429b698
VadimArkhipov <arkhipov_vadim@inbox.ru>
2023-02-28 21:36:25 +0300

    lab2: done

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import sys
 from PyQt6.QtWidgets import QApplication
 from MainWindow import MainWindow
 
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     main_window = MainWindow()
-    main_window.setFixedSize(957, 671)
+    main_window.setFixedSize(1017, 700)
     sys.exit(app.exec())
