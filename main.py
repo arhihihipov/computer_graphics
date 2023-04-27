b0410845629da8e60d9a29f2351276042a9420c0
VadimArkhipov <arkhipov_vadim@inbox.ru>
2023-04-27 23:11:09 +0300

    broken

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt6.QtWidgets import QApplication
-from MainWindow import MainWindow
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    main_window = MainWindow()
-    main_window.setFixedSize(975, 679)
-    sys.exit(app.exec())
